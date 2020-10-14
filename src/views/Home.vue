75c15a17ad4456fee49f3a8f67e18fe2170f23c3
AngelSum233 <1347486073@qq.com>
2020-10-14 09:53:28 +0800

    删除一些不要的组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,15 @@
 <template>
   <div>
-    <home-header :city="city"></home-header>
-    <home-swipe :list="swiperList"></home-swipe>
-    <home-icons :list="iconList"></home-icons>
-    <home-recommend :list="recommendList"></home-recommend>
+    我是首页
     <bottomNavigation></bottomNavigation>
   </div>
 </template>
 
 <script>
-import HomeHeader from '../components/Header'
-import HomeSwipe from '../components/Swipe'
-import HomeIcons from '../components/Icons'
-import HomeRecommend from '../components/Recommend'
-import bottomNavigation from '../components/bottomNavigation' //
+import bottomNavigation from '../components/bottomNavigation'
 export default {
-  name: 'Home',
   components: {
-    HomeHeader: HomeHeader,
-    HomeSwipe: HomeSwipe,
-    HomeIcons: HomeIcons,
-    HomeRecommend: HomeRecommend,
     bottomNavigation
-  },
-  data: function () {
-    return {
-      city: '',
-      swiperList: [],
-      iconList: [],
-      recommenList: []
-    }
-  },
-  getHomeInfoSucc: function (res) {
-    res = res.data
-    if (res.ret && res.data) {
-      this.city = res.data.city
-      this.swiperList = res.data.swiperList
-      this.iconList = res.data.iconList
-      this.recommendList = res.data.recommendList
-    }
   }
 }
 </script>
